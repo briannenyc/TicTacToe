96b11c7dbf4593a6ab242eecaf6d776e3deada22
Brianne Nunnelley <brianne.nyc@mac.com>
2022-01-31 20:34:36 -0600

    functionality complete

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
 const gameState = {
-    players: ['x', 'o'],
-    currentPlayer: 'x',
-    b1: null,
-    b2: null,
-    b3: null,
-    b4: null,
-    b5: null,
-    b6: null,
-    b7: null,
-    b8: null,
-    b9: null,
+    players: ['X', 'O'],
+    currentPlayer: 'X',
+    
 }
 
 const b1 = document.getElementById('box1');
@@ -26,6 +18,7 @@ const outro = document.getElementById('outro');
 const reset = document.getElementById('reset');
 const playerForm = document.getElementById('playerForm')
 const playerInput = document.getElementById('playerInput');
+const playerTurn = document.getElementById('playerTurn')
 const submitButton = document.getElementById('submitButton');
 
 const btn = document.getElementsByClassName('btn');
@@ -42,16 +35,20 @@ intro.innerText = `Let's Play Tic Tac Toe, ${playerName}!`;
 playerInput.value = "";
 })
 
-submitButton.addEventListener ('submit', function(event){
-    console.log("this works");
-})
-
+reset.addEventListener('click', resetGame )
+   
 function resetGame(){
-    return gameState;
-console.log("this works")
-
-
-}
+    console.log("this works now too");
+    outro.innerText = "Click to Play!";
+    playerTurn.innerText = "";
+    gameState.currentPlayer = 'X';
+    for (var i = 0 ; i < Object.keys(btn).length; i++){
+        btn[i].innerText = "-";
+        btn[i].disabled = false; 
+    // for (var i = 0 ; i < Object.keys(btn).length; i++) {
+        
+    
+}}
 
 
 for (var i = 0 ; i < Object.keys(btn).length; i++) {
@@ -63,37 +60,27 @@ function play(event){
       
       checkWinner();
       
-      
       switchPlayer();
+
+      checkButton ();
 }
 
 function switchPlayer (){
-    if (gameState.currentPlayer === 'x'){
-        gameState.currentPlayer = 'o';
-} else { gameState.currentPlayer = 'x'}
+    if (gameState.currentPlayer === 'X'){
+        gameState.currentPlayer = 'O';
+} else { gameState.currentPlayer = 'X'}
+
+    playerTurn.innerText = "Your turn " + gameState.currentPlayer;
+
 }
 
-/* Start with player x
-player x inputs into an empty box, check to see if winning array is met
-if it is, you win, else, switch to player o
-player o inputs into an empty box, check to see if winning array is met
-if it is, you win, else, swtich to player x
-Repeat until winning array is met
-if no winning array is met, you lose
-restart game.
-
-a winning array =
-// box 1, 2, 3 win [0, 1, 2]
-// box 4, 5, 6 win [3, 4, 5]
-// box 7, 8, 9 win [6, 7, 8]
-// box 1, 4, 7 win [0, 3, 6]
-// box 2, 5, 8 win [1, 4, 7]
-// box 3, 6, 9 win [2, 5, 8]
-// box 1, 5, 9 win [0, 4, 8]
-// box 3, 5, 7 win [2, 4, 6]
-*/
-
-
+function checkButton (){
+    for (var i = 0 ; i < Object.keys(btn).length; i++) {
+        if (btn[i].innerText !== "-"){
+          btn[i].disabled = true;  
+        }
+    }
+}
 
 function checkWinner () {
     if (b1.innerText === gameState.currentPlayer && b2.innerText === gameState.currentPlayer && b3.innerText === gameState.currentPlayer){
@@ -115,3 +102,23 @@ function checkWinner () {
     } else {
         outro.innerText = "No Winner! Keep Playing!";}
 }
+
+/* Start with player x
+player x inputs into an empty box, check to see if winning array is met
+if it is, you win, else, switch to player o
+player o inputs into an empty box, check to see if winning array is met
+if it is, you win, else, swtich to player x
+Repeat until winning array is met
+if no winning array is met, you lose
+restart game.
+
+a winning array =
+// box 1, 2, 3 win [0, 1, 2]
+// box 4, 5, 6 win [3, 4, 5]
+// box 7, 8, 9 win [6, 7, 8]
+// box 1, 4, 7 win [0, 3, 6]
+// box 2, 5, 8 win [1, 4, 7]
+// box 3, 6, 9 win [2, 5, 8]
+// box 1, 5, 9 win [0, 4, 8]
+// box 3, 5, 7 win [2, 4, 6]
+*/
